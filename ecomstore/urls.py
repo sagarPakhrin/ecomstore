d2a3a28e831ce84daf54b298c4b7a787b150e99a
sagar <sagarlama826@gmail.com>
2019-07-04 22:58:01 +0545

    as

--- ecomstore/urls.py
+++ ecomstore/urls.py
@@ -15,8 +15,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
         """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+
